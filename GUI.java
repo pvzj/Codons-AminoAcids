9b7e1c4063ba779c11fa1e4949a578ceb77b92f8
pvzj <pvzjeremy@hotmail.com>
2023-01-19 15:00:36 -0800

    broken GUI

---
+++ GUI.java
@@ -0,0 +1,39 @@
+//https://www3.ntu.edu.sg/home/ehchua/programming/java/j4a_gui.html
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends Frame {
+    private Label label;
+    private TextField tf;
+    private Button button;
+    private String input;
+
+    public GUI() {
+        setLayout(new FlowLayout());
+        label = new Label("Codon:");
+        add(label);
+
+        tf = new TextField("Input Codon");
+        tf.setEditable(true);
+        add(tf);
+
+        button = new Button("Submit");
+        add(button);
+
+        setTitle("Hi Vehd");
+        setSize(300,100);
+        setVisible(true);
+       
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    // private class BtnCountListener implements ActionListener {
+    //     @Override
+    //     public void actionPerformed(ActionEvent event) {
+    //         input = tf.getName();
+    //     }
+    // }
+}
